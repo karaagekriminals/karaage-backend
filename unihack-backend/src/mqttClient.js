3b412f05ac63512f488fdc7487217e0c820390a9
Kevin Imvoo <imvooz@gmail.com>
2018-07-29 04:39:08 +1000

    Add support for two sensors for data collection.

--- unihack-backend/src/mqttClient.js
+++ unihack-backend/src/mqttClient.js
@@ -3,10 +3,82 @@
 *  MQTT Client for retrieving and storing data from devices
 *  Author: Tyler Goodwin
 */
-const mqtt = require('mqtt');
+const mqtt = require("mqtt");
 
-function MqttClient(deviceStore, config){
+var AHRS = require("./ahrs");
+var madgwick1 = new AHRS({
+  /*
+   * The sample interval, in Hz.
+   */
+  sampleInterval: 20,
 
+  /*
+   * Choose from the `Madgwick` or `Mahony` filter.
+   */
+  algorithm: "Madgwick",
+
+  /*
+   * The filter noise value, smaller values have
+   * smoother estimates, but have higher latency.
+   * This only works for the `Madgwick` filter.
+   */
+  beta: 0.4,
+
+  /*
+   * The filter noise values for the `Mahony` filter.
+   */
+  kp: 0.5,
+  ki: 0
+});
+
+var madgwick2 = new AHRS({
+  /*
+   * The sample interval, in Hz.
+   */
+  sampleInterval: 20,
+
+  /*
+   * Choose from the `Madgwick` or `Mahony` filter.
+   */
+  algorithm: "Madgwick",
+
+  /*
+   * The filter noise value, smaller values have
+   * smoother estimates, but have higher latency.
+   * This only works for the `Madgwick` filter.
+   */
+  beta: 0.4,
+
+  /*
+   * The filter noise values for the `Mahony` filter.
+   */
+  kp: 0.5,
+  ki: 0
+});
+
+let lastTimestamp = null;
+
+function toRadians(angle) {
+  return angle * (Math.PI / 180);
+}
+
+function toDegrees(angle) {
+  return angle * (180 / Math.PI);
+}
+
+function gravityCompensate(q, acc) {
+  let g = [0.0, 0.0, 0.0];
+
+  // get expected direction of gravity
+  g[0] = 2 * (q[1] * q[3] - q[0] * q[2]);
+  g[1] = 2 * (q[0] * q[1] + q[2] * q[3]);
+  g[2] = q[0] * q[0] - q[1] * q[1] - q[2] * q[2] + q[3] * q[3];
+
+  // compensate accelerometer readings with the expected direction of gravity
+  return [acc[0] - g[0], acc[1] - g[1], acc[2] - g[2]];
+}
+
+function MqttClient(deviceStore, config) {
   const options = {
     clientId: "mqtt-to-http-adapter",
     username: config.mqtt.username,
@@ -21,41 +93,94 @@ function MqttClient(deviceStore, config){
 
   var devices = deviceStore;
 
-  const client = mqtt.connect(`mqtt://${config.mqtt.host}:${config.mqtt.port}`, options);
+  const client = mqtt.connect(
+    `mqtt://${config.mqtt.host}:${config.mqtt.port}`,
+    options
+  );
 
-  client.on('connect', () => {
+  client.on("connect", () => {
     let topicStrs = Object.values(topics);
     console.log(`[MQTT Client]\tSubscribing to: ${topicStrs}`);
     client.subscribe(topicStrs);
   });
 
-  client.on('message', (topic, message) => {
-    let { baseTopic, deviceId, extras} = splitTopic(topic);
+  client.on("message", (topic, message) => {
+    let { baseTopic, deviceId, extras } = splitTopic(topic);
 
     //Parse oclet stream to object
     let data;
     try {
-      data = JSON.parse(Buffer.from(message).toString('utf8'));
-    }
-    catch(e){
+      data = JSON.parse(Buffer.from(message).toString("utf8"));
+    } catch (e) {
       console.error(`[MQTT Client]\tError while parsing JSON: ${e}`);
       return;
     }
 
-    if(baseTopic === 'telemetry' && isValidTelemetry(data)){
+    if (baseTopic === "telemetry" && isValidTelemetry(data)) {
       //Add sensor group to payload
       data.sensor = extras[0];
+
+      if (lastTimestamp == null) {
+        lastTimestamp = data.timestamp;
+      }
+
+      let deltaTime = (data.timestamp - lastTimestamp) / 1000;
+      if (data != null && data.gyro != null && data.gyro.x != null) {
+        let euler;
+        let quat;
+
+        if (deviceId == "98072d27a984") {
+          madgwick1.update(
+            toRadians(data.gyro.x.degPerSecond),
+            toRadians(data.gyro.y.degPerSecond),
+            toRadians(data.gyro.z.degPerSecond),
+            data.accel.x.G,
+            data.accel.y.G,
+            data.accel.z.G,
+            data.mag.x.raw,
+            data.mag.y.raw,
+            data.mag.z.raw,
+            (deltaTimeSec = deltaTime)
+          );
+
+          euler = madgwick1.getEulerAngles();
+          quat = madgwick1.getQuaternion();
+        } else {
+          madgwick2.update(
+            toRadians(data.gyro.x.degPerSecond),
+            toRadians(data.gyro.y.degPerSecond),
+            toRadians(data.gyro.z.degPerSecond),
+            data.accel.x.G,
+            data.accel.y.G,
+            data.accel.z.G,
+            data.mag.x.raw,
+            data.mag.y.raw,
+            data.mag.z.raw,
+            (deltaTimeSec = deltaTime)
+          );
+
+          euler = madgwick2.getEulerAngles();
+          quat = madgwick2.getQuaternion();
+        }
+
+        client.publish("euler/" + deviceId, JSON.stringify(euler));
+        client.publish("quat/" + deviceId, JSON.stringify(quat));
+
+        lastTimestamp = data.timestamp;
+      }
+      //
+
       //store validated data
       devices.pushSensorData(deviceId, data);
       return;
     }
 
-    if(baseTopic === 'configuration' && isValidConfig(data)){
+    if (baseTopic === "configuration" && isValidConfig(data)) {
       devices.updateConfig(deviceId, data);
       return;
     }
 
-    if(baseTopic === 'status' && isValidStatus(data)){
+    if (baseTopic === "status" && isValidStatus(data)) {
       devices.updateStatus(deviceId, data);
       return;
     }
@@ -67,40 +192,40 @@ function MqttClient(deviceStore, config){
 }
 
 // Verify the submitted data is valid
-function isValidTelemetry(data){
-  if(data.timestamp === undefined){
+function isValidTelemetry(data) {
+  if (data.timestamp === undefined) {
     return false;
   }
 
-  return true
+  return true;
 }
 
-function isValidConfig(data){
-  if(data === undefined) return false;
+function isValidConfig(data) {
+  if (data === undefined) return false;
 
-  return true
+  return true;
 }
 
-function isValidStatus(data){
-  if(data === undefined) return false;
+function isValidStatus(data) {
+  if (data === undefined) return false;
 
-  return true
+  return true;
 }
 
 //Split topic to retrieve base and deviceid
-function splitTopic(topic){
-  var splitTopic = topic.split('/');
+function splitTopic(topic) {
+  var splitTopic = topic.split("/");
   var baseTopic = splitTopic[0];
-  var deviceId = '';
+  var deviceId = "";
   var extras = [];
 
-  if(splitTopic.length > 1){
+  if (splitTopic.length > 1) {
     deviceId = splitTopic[1];
   }
-  if(splitTopic.length > 2){
+  if (splitTopic.length > 2) {
     extras = splitTopic.slice(2);
   }
-  return { baseTopic, deviceId, extras}
+  return { baseTopic, deviceId, extras };
 }
 
 module.exports = MqttClient;
--- unihack-backend/src/mqttServer.js
+++ unihack-backend/src/mqttServer.js
@@ -3,65 +3,63 @@
  *  MQTT Broker for facilitating device communication
  *  Author: Tyler Goodwin
  */
-const mosca = require('mosca');
-const ascoltatori = require('ascoltatori');
-const Auth = require('./utils/auth');
-const http = require('http');
+const mosca = require("mosca");
+const ascoltatori = require("ascoltatori");
+const Auth = require("./utils/auth");
+const http = require("http");
 const httpServ = http.createServer();
 
 function MqttServer(db, config) {
-    const auth = new Auth(db, config);
+  const auth = new Auth(db, config);
 
-    const authenticate = function(client, username, password, callback) {
-      if(!username || !password) return callback(null, false);
+  const authenticate = function(client, username, password, callback) {
+    if (!username || !password) return callback(null, false);
 
-      if(username === config.mqtt.username){
-        //Super User
-        let authorized = (username === config.mqtt.username &&
-          password.toString() === config.mqtt.password);
+    if (username === config.mqtt.username) {
+      //Super User
+      let authorized = username === config.mqtt.username && password.toString() === config.mqtt.password;
+      if (authorized) client.user = username;
+      callback(null, authorized);
+    } else {
+      // Single device user
+      auth.authenticate(username, password.toString(), authorized => {
         if (authorized) client.user = username;
         callback(null, authorized);
-      }
-      else {
-        // Single device user
-        auth.authenticate(username, password.toString(), authorized => {
-          if(authorized) client.user = username;
-          callback(null, authorized);
-        });
-      }
+      });
     }
+  };
 
-    const authorizePublish = function(client, topic, payload, callback) {
-      deviceId = topic.split('/')[1];
-      callback(null, client.user === config.mqtt.username || client.user === deviceId);
-    }
+  const authorizePublish = function(client, topic, payload, callback) {
+    deviceId = topic.split("/")[1];
+    callback(null, client.user === config.mqtt.username || client.user === deviceId);
+  };
 
-    const authorizeSubscribe = function(client, topic, callback){
-      deviceId = topic.split('/')[1];
-      callback(null, client.user === config.mqtt.username || client.user === deviceId);
-    }
+  const authorizeSubscribe = function(client, topic, callback) {
+    deviceId = topic.split("/")[1];
+    callback(null, client.user === config.mqtt.username || client.user === deviceId);
+  };
 
-    const settings = {
-      port: config.mqtt.port
-    }
+  const settings = {
+    port: config.mqtt.port
+  };
 
-    var server = new mosca.Server(settings);
+  var server = new mosca.Server(settings);
 
-    server.on('clientConnected', client => {
-      console.log("[MQTT Server]\tClient connected: ", client.id);
-    })
+  server.on("clientConnected", client => {
+    console.log("[MQTT Server]\tClient connected: ", client.id);
+  });
 
-    server.on('ready', () => {
-      // Override auth
-      server.authenticate = authenticate;
-      server.authorizePublish = authorizePublish;
-      server.authorizeSubscribe = authorizeSubscribe;
-      console.log(`[MQTT Server]\tStarting MQTT server on mqtt://localhost:${config.mqtt.port}`);
-    });
+  server.on("ready", () => {
+    // Override auth
+    server.authenticate = authenticate;
+    server.authorizePublish = authorizePublish;
+    server.authorizeSubscribe = authorizeSubscribe;
+    console.log(`[MQTT Server]\tStarting MQTT server on mqtt://localhost:${config.mqtt.port}`);
+  });
 
-    server.attachHttpServer(httpServ);
-    httpServ.listen(config.mqtt.websocket);
-    console.log(`[MQTT Server]\tMQTT server listening on ws://localhost:${config.mqtt.websocket}`);
+  server.attachHttpServer(httpServ);
+  httpServ.listen(config.mqtt.websocket);
+  console.log(`[MQTT Server]\tMQTT server listening on ws://localhost:${config.mqtt.websocket}`);
 }
 
 module.exports = MqttServer;
